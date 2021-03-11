938c22a68a03ffe393da054f10dbad6c380dff10
Riccardo Geri <geririccardo34@gmail.com>
2021-03-11 21:28:05 +0100

    Add change size functionality (still bugged though)

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+const changeSizeButton = document.getElementById("change-size-btn");
 const grid = document.getElementById("grid")
 
 window.addEventListener("load", LoadGrid(16));
+changeSizeButton.addEventListener("click", ChangeSize);
 
 function LoadGrid(dim){
     grid.style.gridTemplateColumns = `repeat(${dim}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${dim}, 1fr)`;
     for(let i = 0; i < dim * dim; i++){
         const gridSquare = document.createElement("div");
         gridSquare.classList = "grid-square";
@@ -12,6 +15,29 @@ function LoadGrid(dim){
     }
 }
 
-function ChangeColor(square) {
-    square.target.style.backgroundColor = "black";
+function ChangeColor(e) {
+    e.target.style.backgroundColor = "black";
+}
+
+function ChangeSize(){
+    let size = prompt("Enter your desired size(1-64)");
+    if(size !== null){
+        size = parseInt(size);
+    }
+    if (size < 1 || size > 64 || Number.isNaN(size)){
+        alert("Invalid size, try again");
+        ChangeSize();
+    }
+    else {
+        ClearCurrentGrid();
+        LoadGrid(size);
+    }
+}
+
+function ClearCurrentGrid(){
+    let gridSquares = document.getElementsByClassName("grid-square");
+    let i = gridSquares.length;
+    while(i--){
+        gridSquares[i].style.backgroundColor = "transparent";
+    }
 }
